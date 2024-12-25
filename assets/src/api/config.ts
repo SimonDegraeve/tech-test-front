e9a526acc9e0f3060afa405b750b625d26ff6691
Simon Degraeve <simon.degraeve@gmail.com>
2024-12-25 20:00:18 +0100

    Update permutations number

--- assets/src/api/config.ts
+++ assets/src/api/config.ts
@@ -2,4 +2,4 @@
 export const apiUrl = 'http://localhost:4000/api'
 
 // Allow to handle many manipulations on the candidate position, value taken from the backend
-export const candidatePositionInterval = 16384
+export const candidatePositionInterval = 16384 // 2^14 allow up to 14 permutations
