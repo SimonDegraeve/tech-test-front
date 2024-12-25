6170da89a363dff7ba0269a0b5b55b66c9c29ba7
Simon Degraeve <simon.degraeve@gmail.com>
2024-12-26 00:33:01 +0100

    Update design of Job list

--- assets/src/pages/JobIndex/index.tsx
+++ assets/src/pages/JobIndex/index.tsx
@@ -1,24 +1,30 @@
 import { Link as RouterLink } from 'react-router-dom'
 import { Link } from '@welcome-ui/link'
+import { Text } from '@welcome-ui/text'
+import { Box } from '@welcome-ui/box'
 import { useJobs } from '../../api'
 
 function JobIndex() {
-  const { isLoading, jobs } = useJobs()
-
-  if (isLoading) {
-    return null
-  }
+  const { jobs } = useJobs()
 
   return (
-    <ul>
-      {jobs?.map(job => (
-        <li key={job.id}>
-          <Link as={RouterLink} to={`/jobs/${job.id}`}>
-            {job.name}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <Box color="neutral-70" backgroundColor="beige-30" p={20} alignItems="center">
+        <Text variant="h5" m={0} maxWidth={1200} margin="auto">
+          List of opened jobs :
+        </Text>
+      </Box>
+
+      <Box as="ul" maxWidth={1200} margin="20 auto" w="100%">
+        {jobs?.map(job => (
+          <li key={job.id}>
+            <Link as={RouterLink} to={`/jobs/${job.id}`}>
+              {job.name}
+            </Link>
+          </li>
+        ))}
+      </Box>
+    </>
   )
 }
 
