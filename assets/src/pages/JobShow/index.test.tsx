6c7e3529088979f5bc7d6114ea6a0cf4b3b0ed16
Simon Degraeve <simon.degraeve@gmail.com>
2024-12-26 01:40:55 +0100

    Add more JobShow tests

--- assets/src/pages/JobShow/index.test.tsx
+++ assets/src/pages/JobShow/index.test.tsx
@@ -1,9 +1,8 @@
 import { describe, expect, test, vi } from 'vitest'
 import { act, fireEvent } from '@testing-library/react'
-
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
 import { render } from '../../test-utils'
 import JobShow from '../../pages/JobShow'
-import { MemoryRouter, Route, Routes } from 'react-router-dom'
 import { candidates, jobs } from '../../test/data'
 import { apiUrl } from '../../api'
 
@@ -40,7 +39,7 @@ describe('JobShow', () => {
     }
   })
 
-  test('update candidate', async () => {
+  test('update candidate to the Right', async () => {
     const updateCandidateSpy = vi.spyOn(window, 'fetch')
     const candidateEmail = candidates[0].email
     const { findByText, findByRole, getByRole, getByTestId } = render(renderComponent())
@@ -68,6 +67,42 @@ describe('JobShow', () => {
     // Ensure the server get the change
     expect(updateCandidateSpy).toHaveBeenCalledWith(`${apiUrl}/jobs/1/candidates/1`, {
       body: '{"candidate":{"id":1,"email":"user1@email.com","position":16384,"status":"interview"}}',
+      headers: { 'Content-Type': 'application/json' },
+      method: 'PATCH',
+    })
+
+    // Ensure the card is in the correct column after server response
+    expect(getByTestId(`column-interview`).contains(getByRole('button', { name: candidateEmail })))
+  })
+
+  test('update candidate to the Left', async () => {
+    const updateCandidateSpy = vi.spyOn(window, 'fetch')
+    const candidateEmail = candidates[3].email
+    const { findByText, findByRole, getByRole, getByTestId } = render(renderComponent())
+
+    // Wait for the candidate to be rendered
+    expect(await findByRole('button', { name: candidateEmail })).toBeInTheDocument()
+
+    // Select card
+    act(() => {
+      fireEvent.keyDown(getByRole('button', { name: candidateEmail }), { code: 'Space' })
+    })
+
+    // Move card to the left column
+    act(() => {
+      fireEvent.keyDown(getByRole('button', { name: candidateEmail }), { code: 'ArrowLeft' })
+    })
+    await findByText('Draggable item 4 was moved over droppable area __empty__-interview.')
+
+    // Commit the change
+    act(() => {
+      fireEvent.keyDown(getByRole('button', { name: candidateEmail }), { code: 'Space' })
+    })
+    await findByText('Draggable item 4 was dropped over droppable area __empty__-interview')
+
+    // Ensure the server get the change
+    expect(updateCandidateSpy).toHaveBeenCalledWith(`${apiUrl}/jobs/1/candidates/1`, {
+      body: '{"candidate":{"id":4,"email":"user4@email.com","position":40960,"status":"interview"}}',
       headers: { 'Content-Type': 'application/json' },
       method: 'PATCH',
     })
